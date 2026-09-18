1322b7b3567b3d787e7b6f0783c5d6a23dea01ad
jordan-tr7 <jordan.tr@northeastern.edu>
2026-09-18 15:57:26 -0400

    testing pymupdf for image detection

--- parse_pdfs.py
+++ parse_pdfs.py
@@ -1,16 +1,31 @@
 
-from pypdf import PdfReader
+#from pypdf import PdfReader
+import pymupdf 
 
 TEST_FILE = "files/inputs/Level-1-Unit-1-printable-Morpheme-tiles.pdf"
 
 def main():
     
-    reader = PdfReader(TEST_FILE)
+    doc = pymupdf.open(TEST_FILE)
+    print(f"Pages: {len(doc)}")
 
-    for i, page in enumerate(reader.pages):
-        text = page.extract_text()
-        print(f"--- Page {i + 1}")
-        print(text)
+    for page_num, page in enumerate(doc):
+
+        images = page.get_images(full=True)
+        text = page.get_text()
+        drawings = page.get_drawings()
+        print(f"\n=== Page {page_num + 1} ===")
+        print(f"{len(images)} image(s)")
+        print(f"{len(drawings)} drawing(s)")
+        #print(text)
+
+
+    # reader = PdfReader(TEST_FILE)
+
+    # for i, page in enumerate(reader.pages):
+    #     text = page.extract_text()
+    #     print(f"--- Page {i + 1}")
+    #     print(text)
 
 
 
